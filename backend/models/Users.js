e48329ba17e91886b4c399d4b8cee1dd6049a66e
Nico <n.rossi.studio@gmail.com>
2026-03-08 17:58:53 +0100

    Users.js -> Completed

---
+++ backend/models/Users.js
@@ -0,0 +1,40 @@
+//Importo mongoose per la gestione dei dati sul DB
+const mongoose = require ("mongoose");
+
+//Creo lo schema di dati chiamato UserSchema per la registrazione delle info dell'utente
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Campo obbligatorio']
+    },
+    surname: {
+        type: String,
+        required: [true, 'Campo obbligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'Campo obbligatorio'], 
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Email non valida'], //Regex, controlla i parametri della mail:
+                                                    // ^	inizio della stringa
+                                                    // \S+	uno o più caratteri non spazi
+                                                    // @	il simbolo @
+                                                    // \.	il punto .
+                                                    // $	fine della stringa
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "producer"],
+        default: "User"
+    },
+}, {timestamps: true});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User; 
