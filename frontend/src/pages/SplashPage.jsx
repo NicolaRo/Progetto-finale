1e3c9e8454703a754ffc0e7bcd1e1701df296725
Nico <n.rossi.studio@gmail.com>
2026-04-27 17:25:07 +0200

    Chore Frontend, installed BrowserRouter, Routes Route from react router dom

---
+++ frontend/src/pages/SplashPage.jsx
@@ -0,0 +1,14 @@
+function SplashPage () {
+    return (
+        <>
+        <h1 className="splashpage-title">
+            Progetto Finale
+        </h1>
+        <div className="logo-container">
+        </div>
+        </>
+    );
+}
+
+
+export default SplashPage;
