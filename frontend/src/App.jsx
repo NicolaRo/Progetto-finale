bf4d9c38bc4895e48643507773fc89fa5693e87b
Nico <n.rossi.studio@gmail.com>
2026-04-29 15:18:37 +0200

    Feature -Login.jsx ->
    a toggle displays the log in or the sign in component
    to access the App.
    
    insight:
    Imported GoogleAuthProvider and signInWithPopup to enable firebase login with google

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
+import LoginPage from './pages/Login';
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* the splashpage will navigate to the components and here represent the entry point of the App */}
-        <Route path="/" element={<SplashPage/>}>
-        </Route>
+        <Route path="/" element={<SplashPage/>}/>
+        <Route path="/Login" element={<LoginPage/>}/>
       </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,54 +1,88 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import {auth} from '../services/firebaseConfig';
+import {useState} from 'react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 
-//Component's main function
-function SignInForm() {
 
-  //Component's state
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
+function LoginPage() {
+    const [showLogin, setShowLogin] = useState(true);
+    const [showUser, setShowUser] = useState(true);
 
-  //sub-component's states
-  const [signIn, setSignInOpen] = useState(false);
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [email, setEmail] = useState("");
 
-  return (
-    <>
-    <button className={`button ${signIn? "active" : ""}`}
-    onClick={() => setSignInOpen(!signIn)}>
-        Sign in
-    </button>
-    {signIn && (
-        <div className="input-container">
-        <h4>Name</h4>
-        <input
-          className="input-textarea"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Ie. John..."
-        />
-        <h4>Surname</h4>
-        <input
-          className="input-textarea"
-          type="text"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-          placeholder="Ie. Doe..."
-        />
-        <h4>Email</h4>
-        <input
-          className="input-textarea"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Ie. john.doe@example.com"
-        />
-      </div>
-    )};
-    </>
-  );
+    const provider = new GoogleAuthProvider();
+
+    //Function to enable google login popup
+    const handleLogIn = async () => {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result.user);
+    }
+    
+    return (
+        <>
+        {/*Toggle to activate sign in form (default is the login screen) */}
+        <button 
+            className="toogle-login"
+            onClick={() => {
+                setShowLogin(!showLogin);
+            }}> 
+            Sign In
+        </button>
+        {showLogin ? (
+            <div className="login-container">
+                <h3 className="container-title">Log In</h3>
+                <button
+                    className="btn-google-auth"
+                    onClick={handleLogIn}> Log In
+                </button>
+        </div>
+        ) : (
+            <div className="signin-container">
+                <h3 className="container-title">Sign In</h3>
+                
+                <div className="img-container">
+                <img 
+                    className="imgage"
+                    src="placeholder-img.png"
+                    alt="illustration"
+                />
+                </div>
+                <input 
+                className="input-text"
+                type="text"
+                value={name}
+                onChange={(e)=> setName(e.target.value)}
+                placeholder="I.e.: John"
+                />
+                <input 
+                    className="input-text"
+                    type="text"
+                    value={surname}
+                    onChange={(e) => setSurname(e.target.value)}
+                    placeholder="I.e.: Doe"
+                />
+                <input 
+                    className="input-text"
+                    type="text"
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                    placeholder="I.e.: john.doe@example.com"
+                />
+            </div>
+            
+        )}
+        <button 
+                className="toogle-user"
+                onClick={() => {
+                    setShowUser(!showUser);
+                }}> 
+                User
+            </button>
+        
+        </>
+    );
 }
 
-export default (SignInForm);
+export default LoginPage;
