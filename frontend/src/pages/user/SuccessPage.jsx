24bd56c63400c7ce7d09b5dbdfd5ecc944e995d5
Nico <n.rossi.studio@gmail.com>
2026-05-11 11:37:54 +0200

    Style
    SuccessPage.jsx has been styled

--- frontend/src/pages/user/SuccessPage.jsx
+++ frontend/src/pages/user/SuccessPage.jsx
@@ -1,5 +1,7 @@
 import {useNavigate} from  "react-router-dom";
 
+import PlaceholderImg from '../../assets/PlaceholderImg.png';
+
 function SuccessPage() {
     const navigate = useNavigate();
 
@@ -7,8 +9,16 @@ function SuccessPage() {
         <>
         <div className="payment-success">
             <h1 className="payment-success-title">🎉 Payment successful!</h1>
+            <img
+                className="payment-success-icon"
+                src={PlaceholderImg}
+                alt="Payment successful"
+                />
             <p>Your order has been confirmed.</p>
-      <button onClick={() => navigate("/UserHome")}>Back to shop</button>
+            <p>You will receive an email when the order is shipped.</p>
+      <button 
+        className="back-btn"
+        onClick={() => navigate("/UserHome")}>Back to shop</button>
         </div>
         </>
     );
