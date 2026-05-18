99a7fd42db743f55a9b3ac59bd9acebdfa624637
Nico <n.rossi.studio@gmail.com>
2026-05-18 16:07:28 +0200

    Style Producer order p1

--- frontend/src/components/ProducerOrderCard.jsx
+++ frontend/src/components/ProducerOrderCard.jsx
@@ -10,7 +10,7 @@ function ProducerOrderCard({
 
   return (
     <div className="single-order-container">
-
+      
       {/* ORDER HEADER */}
       <div className="order-user-details-container">
         <p><strong>Order ID:</strong> {order._id}</p>
@@ -21,13 +21,17 @@ function ProducerOrderCard({
       {/* PRODUCTS + PACK */}
       {order.products?.map((p) => (
         <div key={p.product._id} className="ordered-products">
+          
           <p><strong>{p.product.name}</strong></p>
           <p>Qty: {p.orderedQuantity}</p>
 
           {(order.status === "Order created" || order.status === "Preparing order") &&
             user?.role === "Producer" && (
             <>
-              <select
+            
+
+
+              <select className="select-container-type"
                 value={containerSelections?.[order._id]?.[p.product._id]?.type || ""}
                 onChange={(e) => handleContainerChange(p.product._id, order._id, "type", e.target.value)}
               >
@@ -37,7 +41,7 @@ function ProducerOrderCard({
                 <option value="Freezer-Container">Freezer</option>
               </select>
 
-              <select
+              <select className="select-container-qty"
                 value={containerSelections?.[order._id]?.[p.product._id]?.quantity || ""}
                 onChange={(e) => handleContainerChange(p.product._id, order._id, "quantity", e.target.value)}
               >
@@ -47,7 +51,7 @@ function ProducerOrderCard({
                 ))}
               </select>
 
-              <button onClick={() => handlePackedProduct(p.product._id, order._id)}>
+              <button className="btn-pack-product" onClick={() => handlePackedProduct(p.product._id, order._id)}>
                 Pack product
               </button>
             </>
@@ -57,7 +61,7 @@ function ProducerOrderCard({
 
       {/* CHECKIN — solo quando lo user ha confermato la ricezione */}
       {order.status === "Order shipped" && (
-        <button onClick={() => handleContainerCheckin(order._id)}>
+        <button className="checkin-containers-btn" onClick={() => handleContainerCheckin(order._id)}>
           Check in containers and close the order
         </button>
       )}
