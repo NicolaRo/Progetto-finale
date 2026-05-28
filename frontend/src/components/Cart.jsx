488076dcce3eb581b7c268735bf447c6c41b3a3c
Nico <n.rossi.studio@gmail.com>
2026-05-28 13:58:27 +0200

    Feature cart displays the container deposit

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -120,7 +120,11 @@ function Cart({ setShowCart }) {
           
           </div>
           <div className="cart-total">
-            <h4>Total: {totalPrice}€</h4>
+          <h5>Order total: {totalPrice.toFixed(2)}€</h5>
+
+            <h6 className="deposit-info">🌱 Refundable container deposit: 5,00€</h6>
+            
+            <h3>Total due today: {(totalPrice + 5).toFixed(2)}€</h3>
           </div>
         </div>
       </div>
