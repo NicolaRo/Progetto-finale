6377460cc7310cafc71e1d08edbe4be02429ff65
Nico <n.rossi.studio@gmail.com>
2026-06-07 13:24:30 +0200

    fix: add Netlify URL to CORS allowed origins

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ const app = express();
 
 //Configure Cors
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173',
+          'https://packback-greengrocery.netlify.app'
+  ]
 }));
 
 //--- MIDDLEWARE ---
